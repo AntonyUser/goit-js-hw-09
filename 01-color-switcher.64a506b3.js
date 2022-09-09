092e5c0e7c79eea39a59d33d75e93a51a6042a61
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2022-09-09 13:40:24 +0000

    Deploying to gh-pages from @ AntonyUser/goit-js-hw-09@b406a67c759f1b2aae06265289ad5b2fe9f5b56f 🚀

---
+++ 01-color-switcher.64a506b3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled","true"),t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.64a506b3.js.map
